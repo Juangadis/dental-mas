553aa33eedd55a48ef1ccfe0f54d60dec35c5373
Juangadis <juanestevecorena98@gmail.com>
2025-01-09 23:58:50 -0300

    Add form and style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -161,6 +161,23 @@
                     </div>
                 </div>
             </section>
+
+            <section id="section-plans" class="bg-gradient-to-b from-blue-100 to-white py-16">
+                <form id="form-checkout">
+                    <div id="form-checkout__cardNumber" class="container"></div>
+                    <div id="form-checkout__expirationDate" class="container"></div>
+                    <div id="form-checkout__securityCode" class="container"></div>
+                    <input type="text" id="form-checkout__cardholderName" />
+                    <select id="form-checkout__issuer"></select>
+                    <select id="form-checkout__installments"></select>
+                    <select id="form-checkout__identificationType"></select>
+                    <input type="text" id="form-checkout__identificationNumber" />
+                    <input type="email" id="form-checkout__cardholderEmail" />
+                
+                    <button type="submit" id="form-checkout__submit">Pagar</button>
+                    <progress value="0" class="progress-bar">Cargando...</progress>
+                </form>
+            </section>
         </main>
         
         <footer class="bg-gray-800 text-white py-8">
